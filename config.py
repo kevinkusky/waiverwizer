946daeac032a55db855225e3e321360233535856
kevinkusky <kevinkusky@gmail.com>
2024-08-27 20:31:44 -0400

    basic react app

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 """config.py"""
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
+    DEBUG = os.environ.get('DEBUG') or True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:password@localhost/waiverwizer'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
